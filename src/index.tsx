8dec470094320ec74a302db498fef1ef5002f8c7
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-23 14:14:30 +0500

    BRR: Initial setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './css/index.css';
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
 
-root.render(
+ReactDOM.render(               // first argument
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store}>         
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+document.getElementById('root')     //second argument
 );
 
 // If you want to start measuring performance in your app, pass a function
