8795d767d4055974d47b2d3096d94fb0611e5d48
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-24 13:48:53 +0500

    fix: develop burak screen based components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,53 @@
 import React from 'react';
 import './css/app.css';
 import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './app/MaterialTheme/styled';
+import { Link, Route, Switch } from 'react-router-dom';
+import { HomePage } from './app/screens/homePage';
+import { ProductsPage } from './app/screens/productsPage';
+import { OrdersPage } from './app/screens/ordersPage';
+import { UserPage } from './app/screens/userPage';
 
 function App() {
-  return <Container sx={{background: "orange" }}>
-    <Stack flexDirection={"column"}>
-      <Box sx={{my: 4}}>
-        <Typography variant='h4' component={'h4'}>
-          Create React App on Typescript with REDUX
-        </Typography>
-      </Box>
-      <Box>
-        <RippleBadge badgeContent={4}>
-        <Button variant="contained">Contained</Button>
-        </RippleBadge>
-      </Box>
-      
-    </Stack>
-  </Container>
+  return (
+    <div>
+    <nav>
+      <ul>
+      <li>
+          <Link to="/">HomePage</Link>
+        </li>
+        <li>
+          <Link to="/products">ProductsPage</Link>
+        </li>
+        <li>
+          <Link to="/orders">OrdersPage</Link>
+        </li>
+        <li>
+          <Link to="/member-page">UserPage</Link>
+        </li>
+        
+      </ul>
+    </nav>
+
+    <Switch>
+      <Route path="/products">
+        <ProductsPage />
+      </Route>
+      <Route path="/orders">
+        <OrdersPage />
+      </Route>
+      <Route path="/members-page">
+        <UserPage />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </Switch>
+  </div>
+  );
 }
+
+
+
+
+
 export default App;
