8b0f9e65186cd3fc334ecf12ea39c27edbe0f692
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-03-29 05:22:11 +0500

    fix: organize homepage related sectional components

---
+++ src/app/screens/homePage/Advertisement.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Advertisement() {
+    return (<div>
+        <Container>Advertisement</Container>
+        </div>
+        );
+}
---
+++ src/app/screens/homePage/Events.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Container } from "@mui/material";
+
+export default function Events() {
+    return (<div>
+        <Container>Events</Container>
+        </div>
+        );
+}
