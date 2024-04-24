72b24303424c61148dc675bd3e2f95e6cdc51df8
sherboev boburmirzo <sherboevboburmirzo@sherboevs-MacBook-Air.local>
2024-04-24 12:58:39 +0900

    feat: develop product service business logic to retrieve data from backend

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,30 +7,40 @@ import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
 import "../../../css/home.css";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch,  } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { createSelector } from "reselect";
 import { setPopularDishes } from "./slice";
-import { retrievePopularDishes } from "./selector";
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 //** REDUX SLICE & SELECTOR */
-const actionDispatch = (dispatch: Dispatch) => ({
+const actionDispatch = (dispatch: Dispatch) => ({ //SLICE
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 });
-const popularDishesRetriever = createSelector(
-  retrievePopularDishes, 
-  (popularDishes) => ({ popularDishes })
-  );
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
-  const { popularDishes } = useSelector(popularDishesRetriever);
+  const { setPopularDishes } = actionDispatch(useDispatch());  // redux storagemizga yukalandigan function
       // Selector: Store => Data  
-    useEffect(() => {}, []);
+  
+    useEffect(() => {
+    // BackEnd server data fetch(request) => DATA
+      const product = new ProductService();
+      product.getProducts({
+        page: 1,
+        limit: 4,
+        order: "productViews",
+        productCollection: ProductCollection.DISH
+      })
+      .then(data => {
+        setPopularDishes(data);
+      })
+      .catch((err) => console.log(err));
+    }, []);
 
-    
-      // BackEnd server data request => DATA
+  
+      
+      
       // Slice: Data => Store
     return (
       <div className={"homepage"}>
